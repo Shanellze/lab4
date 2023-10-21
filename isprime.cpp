7f0c37bdae7d1f526fd082b235b00eb562e8398e
Shanell Pereira <shanell.pereira@yahoo.co.uk>
2023-10-21 22:50:02 +0100

    Fixed bug - Corrected the calculation and added a base case.

--- isprime.cpp
+++ isprime.cpp
@@ -1,18 +1,32 @@
 #include <iostream>
 
+
+// test divisors of number, if a divisor other than 1 and number is
+// found, then number is not prime.
+
+
 int main(){
 
   int number;
+
+  //Asks for the user input
   std::cout << "Enter an integer: ";
   std::cin >> number;
 
-  // test divisors of number, if a divisor other than 1 and number is
-  // found, then number is not prime.
+  //initial value
   bool isPrime = true;
-  for (int i = 2; i <= number; i++)
-    if (!(number % i))
+
+  //Base case
+  if (number<=1)
+    isPrime = false;
+
+  //A prime number is a number that can only be divided by itself 
+  //and 1 without remainders. 
+  for (int i = 2; i <= number/2; i++)
+    if ((number % i)==0)
       isPrime = false;
 
+  //Responds with a valid response if the number is prime.
   if (isPrime)
     std::cout << "Prime\n";
   else
